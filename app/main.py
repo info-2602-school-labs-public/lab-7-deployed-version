34a9d92f98887baf8a287050e3a9884e8c06d4c6
Luminescent-Vertigo <kadiebisnathuwi003@gmail.com>
2026-04-01 18:36:06 -0400

    modifications to accomodate admin users

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ from starlette.middleware.sessions import SessionMiddleware
 from app.routers import templates, static_files, router, api_router
 from app.config import get_settings
 from contextlib import asynccontextmanager
-
+from app.cli import seed_data
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     from app.database import create_db_and_tables
     create_db_and_tables()
+    seed_data()    # to load seed data on app startup (comment out if not needed)
     yield
-
 
 
 app = FastAPI(middleware=[
